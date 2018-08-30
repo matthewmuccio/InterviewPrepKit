161b9afa512adb61d884b1e1bbfedc40cc210be0
Matthew Muccio <me@matthewmuccio.com>
2018-08-29 23:09:32 -0700

    Adds challenge code for Arrays exercise 1.

---
+++ arrays1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+import os
+
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    sum = []
+    for i in range(len(arr) - 2):
+        for j in range(len(arr) - 2):
+            sum.append(arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2])
+    return max(sum)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    arr = []
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+    result = hourglassSum(arr)
+    fptr.write(str(result) + '\n')
+    fptr.close()
