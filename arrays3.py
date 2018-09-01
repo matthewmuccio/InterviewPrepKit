62b7330f688c84d2152d84c5989844c97cc0678a
Matthew Muccio <me@matthewmuccio.com>
2018-08-31 23:48:28 -0400

    Adds challenge code for Arrays exercise 3.

---
+++ arrays3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+def minimumBribes(q):
+    bribes = 0
+    for i in range(len(q) - 1, -1, -1):
+        if q[i] - (i + 1) > 2:
+            print("Too chaotic")
+            return
+        for j in range(max(0, q[i] - 2), i):
+            if q[j] > q[i]:
+                bribes += 1
+    print(bribes)
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for t_itr in range(t):
+        n = int(input())
+        q = list(map(int, input().rstrip().split()))
+        minimumBribes(q)
